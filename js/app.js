be751ad0797350e09e80d771484732d6fce608cd
Sayem Hossain <sayemhossain1823@gmail.com>
2024-12-11 21:52:10 -0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+// search sm
+$('#smSearchBtn').click(function(){
+    $('.searchSm').addClass('active')
+})
+$('.searchSm .closeBtn').click(function(){
+    $('.searchSm').removeClass('active')
+})
+
+
+// mobile bottom header
+$('.mobileBottomIcon').click(function(){
+    $('#mobileBottom').addClass('.active')
+})
+
+
+
+
+
+})
